3ce6e75b9c20037956cff064817d43e8962d15cf
neallylove <107135328+NurHidayah-85@users.noreply.github.com>
2023-04-02 10:00:49 +0800

    new

--- HospitalApplication.java
+++ HospitalApplication.java
@@ -6,10 +6,12 @@
 package patient;
 
 /**
- *
- * @author LENOVO
+ * POST LAB 2
+ * SUBMISSION : 28 MARCH 2023
+ * @author NUR HIDAYAH BINTI ISHAK 2020496204 CSC2304C
  */
 import java.util.*;//this will include all classes from java.util package
+import java.util.ArrayList;
 
 
 
@@ -23,35 +25,47 @@ public static void main (String[] args)
         String patientAddress;	//patient address
         String typeRoom;      // type of room 
         int noDay;            // no of day
-        
+        int numpatient;
         
         Scanner s = new Scanner (System.in);// Create a Scanner object as input
-        Patient [] p = new Patient[2];  // declare an array p with the size of 50
         
-         for (int i =0; i<p.length; i++) 
+       //use this coding toe insert number of patients in manual
+        //System.out.print("Please enter Number of patients : ");
+        //numpatient = Integer.parseInt(s.nextLine());
+        //answer b(i)
+        //Patient [] p = new Patient[numpatient];  // declare an array p with the size of 50
+        
+        
+        //answer b(i)
+        Patient [] p = new Patient[50];  // declare an array p with the size of 50
+        
+        
+        
+        
+        for (int i =0; i<p.length; i++) 
         { // p[i] = new Patient(); 
             System.out.println ("Patient "+ (i+1)); 
             System.out.println ("Enter name: "); //patient name
-            patientName = s.next(); 
-            System.out.println ("Enter number: "); //patient number
-            patientNumber = s.nextInt(); 
+            patientName = s.nextLine(); 
+            System.out.println ("Enter number: (ex:0176842082) "); //patient number
+            patientNumber = Integer.parseInt(s.nextLine());
             System.out.println ("Enter address: "); //patient address
-            patientAddress = s.next(); 
-            System.out.println ("Enter Room type: "); //patient room type
-            typeRoom = s.next(); 
+            patientAddress = s.nextLine(); 
+            System.out.println ("Enter Room type: (Please choose one selection - Diamond/Gold/Silver/Bronze) "); //patient room type
+            typeRoom = s.nextLine(); 
             System.out.println ("Enter number of day(s): "); //patient room type
-            noDay = s.nextInt(); 
+            noDay = Integer.parseInt(s.nextLine());
             
-           p[i] = new Patient (patientName, patientNumber,patientAddress);
-           p[i] = new Room (typeRoom,noDay, patientName, patientNumber,patientAddress);
+            p[i] = new Patient (patientName, patientNumber,patientAddress);
+            p[i] = new Room (typeRoom,noDay, patientName, patientNumber,patientAddress);
               
         }
-         for (int i=0; i<p.length; i++) {
-        System.out.println(p[i]);//display the details information of  patient and room.
-
-        }
-    
-        // B ii)
+         
+        
+        //answer b(ii) 
+        //count the number of patients who stay in the hospital for more 
+        //than twenty (20) days.
+         
         int patientCount = 0;
         for (int i=0; i<p.length; i++) {
             // Convert patient to Room object
@@ -61,16 +75,18 @@ public static void main (String[] args)
                 patientCount++;
             }
         }
-
-        System.out.println(patientCount + " have stayed for more than 20 days.");
-       //  for (int i=0; i<p.length; i++) {
-       //     if (p.getoDay => 20) {
-       //         System.out.println(p[i]);
-       //     } else {
-       //         System.out.println("");
-        //    }
-        //}
-         
-         
+        
+        //display the number of patients who stay in the hospital for more 
+        //than twenty (20) days.
+        System.out.println ("\n");
+        System.out.println(patientCount + " have stayed for more than 20 days.\n");
+        
+        //answer b(iii)
+        //display the average payment per day of each patient
+        System.out.println("The average payment per day collected from all patients are:");
+         for (int i=0; i<p.length; i++) {
+             System.out.println(p[i]);
+         }
+      
     }
 }
